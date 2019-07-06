41459fca883415f01eb8a1904fde2a65c6837118
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-07-06 16:20:47 -0400

    Add visual test of Math.random

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 // Import stylesheets
 import './style.css';
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = '<h1>TypeScript Starter - random</h1>';
+const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
+
+ctx.fillStyle = 'black';
+
+for (let y = 0; y < 2048; y++) {
+  for (let x = 0; x < 2048; x++) {
+    if (getRandomIntInclusive(0, 1) === 1) {
+      ctx.fillRect(x, y, 1, 1)
+    }
+  }
+}
+
+
+function getRandomIntInclusive(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+
